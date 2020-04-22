a4d4e225817c99dd3619de3c5b6d564666905af0
The4thJuliek <58864444+The4thJuliek@users.noreply.github.com>
2020-04-22 23:29:14 +0530

    Add files via upload

---
+++ Week 2/Week2_usainbolt.m
@@ -0,0 +1,13 @@
+% ub_time_secs = 9.58;
+% ub_distance_m = 100;
+% ub_time_min = ub_time_secs/60
+% ub_time_min_kms = ub_time_min * 1000
+hundred = 100/9.58 * 18/5
+
+
+
+
+% kek_time_hr = 121.39;
+% kek_distance_km = 42.195;
+% kek_time_hr/kek_distance_km
+marathon = 42.195/(2 + 1/60 + 39/(60 * 60))
